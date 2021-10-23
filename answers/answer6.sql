1327aaeef0f270e2a9bca11b7938f9551cfedb9f
Nelson de Sousa <73802941+NelsonSSoares@users.noreply.github.com>
2021-10-23 14:14:43 -0300

    resposta 6

---
+++ answers/answer6.sql
@@ -0,0 +1,9 @@
+## 6- Qual fase é a fase quem tem o maior numero de curtidas?
+
+select * from courses;
+select * from likes;
+
+
+select  title as Fase, count(likes.id) as Likes from courses as map
+inner join likes 
+on map.id = likes.id;
